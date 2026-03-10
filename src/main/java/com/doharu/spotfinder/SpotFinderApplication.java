167fc2be2a6eff75fecc707f3780094121e3ae3a
do-haru <doharu05@naver.com>
2026-03-10 15:16:51 +0900

    feat: Spring Boot 백엔드 프로젝트 초기 생성

---
+++ src/main/java/com/doharu/spotfinder/SpotFinderApplication.java
@@ -0,0 +1,13 @@
+package com.doharu.spotfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doharu/spotfinder/SpotFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doharu.spotfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
